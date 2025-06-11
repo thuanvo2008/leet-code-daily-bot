2415a081be9d7c118b8df58d766fcf882bc5318a
steven-vo-wisq <thuanvip322@gmail.com>
2025-06-11 15:02:11 +0700

    improvement: remove code not used

--- main.go
+++ main.go
@@ -16,19 +16,6 @@ func main() {
 		logger.Fatalf("Failed to initialize service: %v", err)
 	}
 
-	//c := cron.New()
-	//
-	//// Schedule to run daily at 9:00 AM
-	//_, err = c.AddFunc("48 11 * * *", dailyBot.ProcessDailyChallenge)
-	//if err != nil {
-	//	logger.Fatalf("Failed to schedule job: %v", err)
-	//}
-	//
-	//logger.Println("Starting LeetCode daily challenge bot")
-	//c.Start()
-
 	dailyBot.ProcessDailyChallenge()
 
-	// Keep the application running
-	select {}
 }
